a94436b07200e9f44ae2f6db67bc6851d11299db
ayesweyen <aswindsunil@gmail.com>
2018-11-14 11:00:08 +0530

    till gmaps

---
+++ app/src/main/java/com/example/amma/travel_guide/GMapsActivity.java
@@ -0,0 +1,52 @@
+package com.example.amma.travel_guide;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmaps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in KOCHI and move the camera
+        LatLng kochi = new LatLng(9.960622, 76.2359853);
+        // Add a marker in KOCHI and move the camera
+        LatLng trv = new LatLng(9.960622, 76.2359853);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(kochi,10));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+       
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        mMap.addMarker(new MarkerOptions().position(kochi).title("Marker in Kerala"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kochi));
+    }
+}
